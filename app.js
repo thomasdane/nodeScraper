f8601874e2bc0d9a726b3c88f51c556ff37c5731
Tom Dane <tjdane@gmail.com>
2015-12-07 22:12:26 +1100

    change url

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var request = require("request"),
 	cheerio = require("cheerio"),
-	url = "http://www.wunderground.com/cgi-bin/findweather/getForecast?&query=" + 02888;
+	url = "http://www.swellnet.com/reports/australia/new-south-wales/eastern-beaches";
 	
 request(url, function (error, response, body) {
 	if (!error) {
 		var $ = cheerio.load(body),
-			temperature = $("[data-variable='temperature'] .wx-value").html();
+			temperature = $('.field-content').html();
 			
 		console.log("It’s " + temperature + " degrees Fahrenheit.");
 	} else {
 		console.log("We’ve encountered an error: " + error);
 	}
 });
+
+////*[@id="curTemp"]/span/span[1]
+//#curTemp > span > span.wx-value
