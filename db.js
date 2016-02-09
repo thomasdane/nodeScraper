3a8382c1c956617fd0afab45952f27fb5b90c58b
Tom Dane <tjdane@gmail>
2016-02-09 19:53:51 +1100

    can start app from node

--- db.js
+++ db.js
@@ -4,8 +4,14 @@ var MongoClient = require('mongodb').MongoClient;
 // Connect to the db using docker host
 MongoClient.connect('mongodb://mongo:27017/blog', function(err, db) {
     
+    
+    //err ? console.log("error") : console.log("no error");
+    
     if(err) {
+        
         console.log(err);
-    } 
-    console.log("Hello world");	
+    } else {
+        console.log("hello")
+    }
+        
 });
