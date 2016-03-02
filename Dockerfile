387ea73364b223c8cbff0bde9e0c792c27711c6b
Tom Dane <tjdane@gmail>
2016-03-02 21:27:49 +1100

    mongo working, node modules ignored

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM nodesource/node:4.0
-
-ADD package.json package.json
-RUN npm install
-ADD . .
-
-CMD ["node","app.js"]
-
--- db.js
+++ db.js
@@ -1,36 +1,14 @@
-var databaseUrl =  "localhost:27017/partywave";
+var databaseUrl =  "localhost/partywave";
 var collections = ["scrapeResults", "targetSites"];
 var mongojs = require("mongojs");
 var db = mongojs(databaseUrl, collections);
 module.exports = db;
 
 module.exports = {
-	save: function (beach) {
-			db.scrapeResults.save({beach: beach}, function(err, saved) {
-			if( err || !saved ) console.log("results not saved");
-			else console.log("report saved")	
-		});
-	}
+  save: function (spot) {
+      db.scrapeResults.save({spot: spot}, function(err, saved) {
+      if( err || !saved ) console.log("results not saved");
+      else console.log("report saved")  
+    });
+  }
 };
-
-
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
-
-// Connection URL
-var url = 'mongodb://localhost:27017/myproject';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
-  //console.log(db)
-  var collection = db.collection('test');
-  collection.insert({test:'test'}, function (err, result) {
-      if (err) {
-          console.log(err)
-      } else {
-          console.log('succcess')        
-      }
-  })
-  db.close();
-});
