8a842c35628a71e178b848f9b3fe53fcfc480ed0
TomDane <tom.dane@ansarada.com>
2016-04-08 23:32:00 +1000

    scrape both sites into one object to create json matching API

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-coastalWatch = require('./coastalWatch.js');
 swellnet = require('./swellnet.js');
 CronJob = require('cron').CronJob;
 
-//scrape websites on a timer
-var job = new CronJob('*/5 * * * * *', function() {
+//scrape websites on a timer 
+new CronJob('*/5 * * * * *', function() {
 	swellnet.scrape();
-	coastalWatch.scrape();
 }, null, true, 'Australia/Sydney')
--- swellnet.js
+++ swellnet.js
@@ -1,29 +1,33 @@
 request = require("request"),
 cheerio = require("cheerio"),
 swellNetUrl = "http://webcache.googleusercontent.com/search?q=cache:http://www.swellnet.com/reports/australia/new-south-wales/eastern-beaches";
-var db = require("./db");
+//coastalWatchUrl = "http://webcache.googleusercontent.com/search?q=cache:http://www.coastalwatch.com/surf-cams-surf-reports/nsw/maroubra";
+db = require("./db");
+
+result = {
+	name: "Eastern Beaches", 
+	reports: []
+}
 
 module.exports = {
 	scrape: function () {
 		request(swellNetUrl, function (error, response, body) {
 			if (!error) {
-					var $ = cheerio.load(body),
-				title = $('.field-content').html();
-				
+				$ = cheerio.load(body),
+				//title = $('.field-content').html();
+				report = {
+							"Name" : "SwellNet",
+							"swellHeight": "1",
+							"swellDirection": "NE",
+							"period": "18s",
+							"windDirection": "W",
+							"windSpeed": "3 Knots",
+							"content": "fishes"
+						}	
 
-				var result = {
-					"Name" : title,
-					"swellHeight": "1",
-					"swellDirection": "NE",
-					"period": "18s",
-					"windDirection": "W",
-					"windSpeed": "3 Knots",
-					"content": "blah"
-				}	
-
-				db.save(result);
-			
+				result.reports.push(report);
 				console.log('scraping swellnet')
+				db.save(result);
 			} else {
 				console.log("We’ve encountered an error: " + error);
 			}
