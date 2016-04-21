9607986667bc8de0a3d149b62b41ad03c097850d
TomDane <tom.dane@ansarada.com>
2016-04-21 22:29:36 +1000

    fix issue with cron - was running on the second

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ scraper = require('./scraper.js');
 CronJob = require('cron').CronJob;
 
 //scrape websites on a timer 
-new CronJob('* * */2 * * *', function() {
+new CronJob('0 0 */2 * * *', function() {
 	scraper.scrapeSwellNet();
 	scraper.scrapeCoastalWatch();
 	scraper.save();
