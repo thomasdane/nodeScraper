fa2e57f36bf698de9a809309179a56eb240c9828
TomDane <tjdane@gmail.com>
2016-05-20 22:39:05 +0000

    change scraping to every 15 mins in morning and arvo, to avoid rate limiting

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ scraper = require('./scraper.js');
 CronJob = require('cron').CronJob;
 
 //scrape websites on a timer 
-new CronJob('0 0 */2 * * *', function() {
+new CronJob('0 */15 6-9,1-3 * * *', function() {
 	scraper.scrapeSwellNet();
 	scraper.scrapeCoastalWatch();
 	scraper.save();
