04acc29cf3d21ade8f2da449e745468e247d1cf1
Tom Dane <tjdane@gmail>
2016-05-22 16:46:47 +1000

    add try catch to prevent problem when report is not present

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ scraper = require('./scraper.js');
 CronJob = require('cron').CronJob;
 
 //scrape websites on a timer 
-new CronJob('0 */15 6-9,1-3 * * *', function() {
+//new CronJob('0 */15 6-9,1-3 * * *', function() {
+//scrape every minute for testing
+new CronJob('0 */1 * * * *', function() {	
 	scraper.scrapeSwellNet();
 	scraper.scrapeCoastalWatch();
 	scraper.save();
