d12b56cad47f82e5af6bac8731e14fadfefb35bd
TomDane <tom.dane@ansarada.com>
2016-05-24 23:14:12 +1000

    remove merge conflicts

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ CronJob = require('cron').CronJob;
 //new CronJob('0 */15 6-9,1-3 * * *', function() {
 //scrape every minute for testing
 new CronJob('0 */1 * * * *', function() {	
->>>>>>> 04acc29cf3d21ade8f2da449e745468e247d1cf1
 	scraper.scrapeSwellNet();
 	scraper.scrapeCoastalWatch();
 	scraper.save();
