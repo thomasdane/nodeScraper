fa4b7022f4d2bdb4c8ff9aa56e61f16657883f27
TomDane <tom.dane@ansarada.com>
2016-05-24 23:02:20 +1000

    add testing timer

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ scraper = require('./scraper.js');
 CronJob = require('cron').CronJob;
 
 //scrape websites on a timer 
-new CronJob('0 */15 6-9,1-3 * * *', function() {
+//new CronJob('0 */15 6-9,1-3 * * *', function() {
+//for testing put cron on faster timer
+new CronJob('0 */1 * * * * *', function() {	
 	scraper.scrapeSwellNet();
 	scraper.scrapeCoastalWatch();
 	scraper.save();
