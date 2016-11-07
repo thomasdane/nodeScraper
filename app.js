55f271805ca82b87b9a70f2d0f249220b03f439c
TomDane <thomasdane@users.noreply.github.com>
2016-11-07 17:10:26 +0000

    remove need to change every daylight savings

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ locationsArray = locations.locationsArray;
 
 //scrape faster for testing
 //new CronJob('*/5 * * * * *', function() {
-new CronJob('0 */30 7,8,9,14,15 * * *', function() {
+new CronJob('0 */30 6,7,8,9,14,15 * * *', function() {
 	locationsArray.forEach(function(location){
 		scraper.scrape(location);
 	});	
