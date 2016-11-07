9104dccb4e5a64441ce2f41597ed33669017a5c8
TomDane <thomasdane@users.noreply.github.com>
2016-11-07 17:46:40 +0000

    create second scrape frequency for summer

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ CronJob = require('cron').CronJob,
 locations = require('./locations.js'),
 locationsArray = locations.locationsArray;
 
-//scrape faster for testing
-//new CronJob('*/5 * * * * *', function() {
-new CronJob('0 */30 6,7,8,9,14,15 * * *', function() {
+//summer (daylight savings)
+new CronJob('0 */30 7,8,9,14,15 * * *', function() {
+//winter
+//new CronJob('0 */30 6,7,8,13,14 * * *', function() {
 	locationsArray.forEach(function(location){
 		scraper.scrape(location);
 	});	
