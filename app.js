2bac95ee58b0e1d0a8ba6b160673a7e225f747f1
TomDane <thomasdane@users.noreply.github.com>
2016-12-20 14:35:02 +0000

    clean up comments

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ CronJob = require('cron').CronJob,
 locations = require('./locations.js'),
 locationsArray = locations.locationsArray;
 
-//summer (daylight savings)
+//summer scrape times
 new CronJob('0 */30 7,8,9,14,15 * * *', function() {
-//winter
+//winter scrape times
 //new CronJob('0 */30 6,7,8,13,14 * * *', function() {
 	locationsArray.forEach(function(location){
 		scraper.scrape(location);
