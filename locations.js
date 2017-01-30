3bd66700f8006b7035c460cc79d4ae5044c80c61
TomDane <thomasdane@users.noreply.github.com>
2017-01-30 17:56:11 +0000

    rename spots

--- locations.js
+++ locations.js
@@ -1,15 +1,6 @@
 var locationsArray = [
 		{
-		name: 'eastern-beaches',
-			urls: {
-				coastalWatch:
-				"http://www.coastalwatch.com/surf-cams-surf-reports/nsw/maroubra"
-				, swellNet: 
-				"http://www.swellnet.com/reports/australia/new-south-wales/eastern-beaches"
-			}
-		}, 
-		{
-		name: 'northern-beaches',
+		name: 'sydney north',
 			urls: {
 				coastalWatch:
 				"http://www.coastalwatch.com/surf-cams-surf-reports/nsw/curl-curl"
@@ -17,13 +8,23 @@ var locationsArray = [
 				"http://www.swellnet.com/reports/australia/new-south-wales/northern-beaches"
 
 			}
-		},{
-		name: 'south-coast',
+		},
+		{
+		name: 'sydney east',
 			urls: {
 				coastalWatch:
-				"http://www.coastalwatch.com/surf-cams-surf-reports/nsw/shoalhaven-heads"
+				"http://www.coastalwatch.com/surf-cams-surf-reports/nsw/maroubra"
+				, swellNet: 
+				"http://www.swellnet.com/reports/australia/new-south-wales/eastern-beaches"
+			}
+		},
+		{
+		name: 'sydney south',
+			urls: {
+				coastalWatch:
+				"http://www.coastalwatch.com/surf-cams-surf-reports/nsw/cronulla"
 				, swellNet:
-				"http://www.swellnet.com/reports/australia/new-south-wales/wollongong"
+				"http://www.swellnet.com/reports/australia/new-south-wales/cronulla"
 			}
 		}
 	]
