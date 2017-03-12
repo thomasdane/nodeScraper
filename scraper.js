758a417c1aa558a9a73d7d38122d55a9d64221d2
Tom Dane <tjdane@gmail.com>
2017-03-12 20:50:40 +0000

    fix issue with empty tides

--- scraper.js
+++ scraper.js
@@ -5,10 +5,8 @@ tides = require("./tides"),
 db = require("./db");
 
 exports.scrape = function (location) {
-
 	var swellNetUrl = location.urls.swellNet;
 	var coastalWatchUrl = location.urls.coastalWatch;
-	
 	var result = {
 		name: location.name, 
 		reports: []
@@ -36,60 +34,57 @@ exports.scrape = function (location) {
 			var sydneyTime = new Date(offset.setHours(serverTime.getHours() + 10));
 
 			//get coastalWatch report
-			var CWcontent = cw('.starLarge').next('.noMarginBottom').text();
-			if (CWcontent) { //check that the scrape is not empty
-				var CWswellDirection = cw('.dir').html();
-				var CWperiod = cw('.swell').children('span').eq(1).html().match(/[0-9]+/);
-				var CWwindSpeed = cw('.wind').children('.val').html();
-				var CWwindDirection = cw('.wind').children('.dir').html();
-				var CWswellHeight = cw('.swell').children('.val').html();
-				var sunrise = cw('.sunrise').html();
-				var sunset = cw('.sunset').html();
-				var tide = tides.getDaylightTides(sunrise, sunset, cw('.tide').text());
-
-				var coastalWatchReport = {
-							"name": "CoastalWatch",
-							"url": location.urls.coastalWatch,
-							"swellHeight": CWswellHeight,
-							"swellDirection": CWswellDirection,
-							"period": CWperiod + "s",
-							"windDirection": CWwindDirection,
-							"windSpeed": CWwindSpeed,
-							"sunrise" : sunrise,
-							"sunset" : sunset, 
-							"tide" : tide,
-							"content": CWcontent,
-							"date": sydneyTime
-							}
-				result.reports.push(coastalWatchReport)
-			} else {console.log('report was not posted yet')}			
+			var cw_content = cw('.starLarge').next('.noMarginBottom').text();
+			var cw_description = cw_content ? cw_content : "Report not posted yet";
+			var CWswellDirection = cw('.dir').html();
+			var CWperiod = cw('.swell').children('span').eq(1).html().match(/[0-9]+/);
+			var CWwindSpeed = cw('.wind').children('.val').html();
+			var CWwindDirection = cw('.wind').children('.dir').html();
+			var CWswellHeight = cw('.swell').children('.val').html();
+			var sunrise = cw('.sunrise').html();
+			var sunset = cw('.sunset').html();
+			var tide = tides.getDaylightTides(sunrise, sunset, cw('.tide').text());
+			var coastalWatchReport = {
+				"name": "CoastalWatch",
+				"url": location.urls.coastalWatch,
+				"swellHeight": CWswellHeight,
+				"swellDirection": CWswellDirection,
+				"period": CWperiod + "s",
+				"windDirection": CWwindDirection,
+				"windSpeed": CWwindSpeed,
+				"sunrise" : sunrise,
+				"sunset" : sunset, 
+				"tide" : tide,
+				"content": cw_description,
+				"date": sydneyTime
+			}
+			result.reports.push(coastalWatchReport)
 
 			//get swellNet report
-			var SNcontent = sn('.views-field-body').children('.field-content').children('p').text();
-			if (SNcontent) {
-				var SNswell = sn('.views-label-nothing').siblings('.field-content').html();
-				var SNswellArray = SNswell.split(/\s(?=[A-Z])/);
-				var SNperiod = sn('.period').html();
-				var SNwind = sn('.views-label-field-surf-report-wind').siblings('.field-content').html();
-				var SNwindArray = SNwind.split(/ /)
-				var swellNetReport = {
-							"name": "SwellNet",
-							"url": location.urls.swellNet,
-							"swellHeight": SNswellArray[0],
-							"swellDirection": SNswellArray[1].replace(/ /g,''),
-							"period": SNperiod,
-							"windDirection": SNwindArray[1],
-							"windSpeed": SNwindArray[0],
-							"sunrise" : sunrise,
-							"sunset" : sunset, 
-							"tide" : tide,
-							"content": SNcontent,
-							"date": sydneyTime
-							}
-				result.reports.push(swellNetReport) 				
-			} else {console.log('report was not posted yet')}			
+			var sn_content = sn('.views-field-body').children('.field-content').children('p').text();
+			var sn_description = sn_content ? sn_content : "Report not posted yet";
+			var SNswell = sn('.views-label-nothing').siblings('.field-content').html();
+			var SNswellArray = SNswell.split(/\s(?=[A-Z])/);
+			var SNperiod = sn('.period').html();
+			var SNwind = sn('.views-label-field-surf-report-wind').siblings('.field-content').html();
+			var SNwindArray = SNwind.split(/ /)
+			var swellNetReport = {
+				"name": "SwellNet",
+				"url": location.urls.swellNet,
+				"swellHeight": SNswellArray[0],
+				"swellDirection": SNswellArray[1].replace(/ /g,''),
+				"period": SNperiod,
+				"windDirection": SNwindArray[1],
+				"windSpeed": SNwindArray[0],
+				"sunrise" : sunrise,
+				"sunset" : sunset, 
+				"tide" : tide,
+				"content": sn_description,
+				"date": sydneyTime
+			}
+			result.reports.push(swellNetReport) 				
 
-			result.reports.length > 0 ? db.save(result) : console.log("report was empty");
+			db.save(result);
 		}
 	});
 }	
