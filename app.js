37e4353bb4698d0569e90d1e8df0e503d68ac2c0
Tom Dane <thomasdane@users.noreply.github.com>
2017-04-03 18:08:54 +0100

    Move to winter scraping times

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ locations = require('./locations.js'),
 locationsArray = locations.locationsArray;
 
 //summer scrape times
-new CronJob('0 */30 7,8,9,14,15 * * *', function() {
+//new CronJob('0 */30 7,8,9,14,15 * * *', function() {
 //winter scrape times
-//new CronJob('0 */30 6,7,8,13,14 * * *', function() {
+new CronJob('0 */30 6,7,8,13,14 * * *', function() {
 	locationsArray.forEach(function(location){
 		scraper.scrape(location);
 	});	
